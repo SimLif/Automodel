bc4ad18f97cae4b5dba6d6dc9800fd1af37e4a18
hqguo <guohaoqiang@baidu.com>
2025-12-13 14:06:14 +0800

    fix: fix type-error in save_checkpoint when multiple validation metrics are present

--- nemo_automodel/recipes/base_recipe.py
+++ nemo_automodel/recipes/base_recipe.py
@@ -214,7 +214,7 @@ class BaseRecipe:
         path = os.path.join(path, f"epoch_{epoch}_step_{step}")
 
         best_val_metric = (
-            val_loss[next(iter(val_loss.keys()) if len(val_loss) == 1 else best_metric_key)] if val_loss else None
+            val_loss[next(iter(val_loss.keys())) if len(val_loss) == 1 else best_metric_key] if val_loss else None
         )
 
         if is_rank_0:
